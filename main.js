7b1414e4e612e32167955f45d8eafeb88c15982b
Alex <alex.mills70@gmail.com>
2015-09-17 10:10:29 -0600

    removed CryptoJS and added a different hash algorithm. Included a hash(hex) to binary function.

--- main.js
+++ main.js
@@ -1,48 +1,26 @@
-function binEncode(data) {
-	//array holds the initial set of un-padded binary results
-	var binArray = []
-
-	//the string to hold the padded results
-	var datEncode = "";
-
-	//encode each character in data to it's binary equiv and push it into an array
-	for (i=0; i < data.length; i++) { 
-		binArray.push(data[i].charCodeAt(0).toString(2)); 
-	} 
-	//loop through binArray to pad each binary entry. 
-	for (j=0; j < binArray.length; j++){ 
-	//pad the binary result with zeros to the left to ensure proper 8 bit binary 
-		var pad = padding_left(binArray[j], '0', 8); 
-		//append each result into a string 
-		// datEncode += pad + ' '; 
-		datEncode += pad;
-	} 
-			//function to check if each set is encoded to 8 bits, padd the left with zeros if not. 
-	function padding_left(s, c, n) { 
-		if (! s || ! c || s.length >= n) {
-			return s;
-		}
-		var max = (n - s.length)/c.length;
-		for (var i = 0; i < max; i++) { s = c + s; } 
-		return s; 
+function binEncode(hex) {
+	var str = "";
+	for(var i=0; i< hex.length; i++){
+		var temp = "0x" + hex[i];
+		temp = parseInt(temp);
+		temp = temp.toString(2);
+		if(hex[i]<8){temp = "0" + temp;}
+		if(hex[i] < 4){temp = "0" + temp;}
+		if(hex[i] < 2){temp = "0" + temp;}
+		str += temp;
 	}
-	//print array of unpadded results in console console.log(binArray); 
-	//string of padded results in console 
-	// console.log(datEncode);
-	return datEncode; 
+	return str;
 }
 
 function hashFun(argument, bits) {
-	var hash = CryptoJS.SHA1(argument);
+	// var hash = CryptoJS.SHA1(argument);
+	// hash = hash.toString(CryptoJS.enc.Base64);
 
-	hash = hash.toString(CryptoJS.enc.Base64);
+	var hash = Sha1.hash(argument);
 	hash = binEncode(hash);
-
-	// console.log(hash.length, hash);
 	hash = hash.substring(hash.length-bits, hash.length);
 	return hash;
 }
-
 
 function collision (bits) {
 	var totalAvg = 0;
@@ -52,24 +30,20 @@ function collision (bits) {
 
 		while(1){
 			var val = Math.random().toString(36).substring(7);
-			// console.log(val);
 			var temp = hashFun(val, bits);
 			tryObj['numTries']++;
 			if(temp in tryObj){
-				console.log("Attempts: ", tryObj['numTries'], "Hash: ", temp);
+				console.log(i, "Attempts: ", tryObj['numTries'], "Hash: ", temp);
 				break;
 			}else{
-				// console.log('adding: ', temp);
 				tryObj[temp] = val;
 			}
-			// console.log(tryObj);
 		}
 		totalAvg += tryObj['numTries'];
 	};
 	console.log("Average Attempts: ", totalAvg/100);
 	console.log("Should be: ", Math.pow(2, bits/2));
 }
-
 
 function preImage(bits) {
 	var valS = Math.random().toString(36).substring(7);
@@ -85,7 +59,7 @@ function preImage(bits) {
 			if(temp === tempS && val.length > 0){
 				console.log(temp, val);
 				console.log(tempS, valS);
-				console.log("Attempts: ", numTries, "Values: ", valS, val, "Hash: ", temp, tempS);
+				console.log(i, "Attempts: ", numTries, "Values: ", valS, val, "Hash: ", temp, tempS);
 				break;
 			}
 		}
@@ -104,5 +78,8 @@ https://code.google.com/p/crypto-js/#SHA-1
 
 CryptoJS for dummies
 http://www.davidebarranca.com/2012/10/crypto-js-tutorial-cryptography-for-dummies/
+
+New Sha1 hash algorithm
+http://www.movable-type.co.uk/scripts/sha1.html
 
 */
